d4f695107f8fc434bb862e13b8ff3f86f13be2dc
Sayakbaev Abai <abaiskii01@gmail.com>
2022-02-05 03:52:13 +0600

    Внедрение и реализация в проект паттернов Медиатор и CQRS

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Notes.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Notes.Application
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellatationToken);
+        Task SaveChangesAsync();
+    }
+}
